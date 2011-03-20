1db3b8c4deca71d77eaf6199e1aa1c788cfcdd20
Helge Rausch <helge@rausch.io>
2011-03-20 11:39:32 +0100

    configuring action mailer hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ Geigercrowd::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => 'geigercrowd.net' }
 end
