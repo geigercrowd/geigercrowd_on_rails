a2238e442e5c8843a766bd7193af783eca2d5cf0
Helge Rausch <helge@rausch.io>
2011-03-20 08:31:18 +0100

    removing redundant routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Geigercrowd::Application.routes.draw do
-  get "location/edit"
-
-  get "location/new"
-
-  get "location/show"
-
-  get "location/delete"
-
   resources :data_types
   resources :samples
   resources :instruments
