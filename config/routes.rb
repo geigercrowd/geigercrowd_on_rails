e1bc7a6f924fc7b67edf8b445157a174f5681d5f
Juan José Vidal <juan.vidal@sponsorpay.com>
2011-05-07 19:19:05 +0200

    formatting devise forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Geigercrowd::Application.routes.draw do
+
   devise_for :users
+
   resources :data_types
   resources :locations
+
   resources :users do
     resources :instruments do
       resources :samples
@@ -15,10 +18,12 @@ Geigercrowd::Application.routes.draw do
   end
 
   root :to => "welcome#index"
+
   match "instruments" => "instruments#list", as: :instruments
   match "samples" => "samples#search", as: :samples, via: :get
   match "samples/find" => "samples#find", as: :find_samples, via: :post
   match "api" => "welcome#api"
   match "api/public" => "welcome#api_public"
   match "api/private" => "welcome#api_private"
+
 end
